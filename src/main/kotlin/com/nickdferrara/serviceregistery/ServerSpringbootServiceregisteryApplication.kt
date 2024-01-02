ecd58fcdf6e81e5116293843b174e55b27ace805
nickdferrara <nick.d.ferrara@gmail.com>
2024-01-01 23:22:33 -0500

    Enable eureka discovery

--- src/main/kotlin/com/nickdferrara/serviceregistery/ServerSpringbootServiceregisteryApplication.kt
+++ src/main/kotlin/com/nickdferrara/serviceregistery/ServerSpringbootServiceregisteryApplication.kt
@@ -2,8 +2,10 @@ package com.nickdferrara.serviceregistery
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class ServerSpringbootServiceregisteryApplication
 
 fun main(args: Array<String>) {
