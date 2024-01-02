5d68d972e94f2eab486230bbf9c78fd3ff195840
nickdferrara <nick.d.ferrara@gmail.com>
2024-01-01 22:56:54 -0500

    Initial check in

---
+++ src/main/kotlin/com/nickdferrara/serviceregistery/ServerSpringbootServiceregisteryApplication.kt
@@ -0,0 +1,11 @@
+package com.nickdferrara.serviceregistery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerSpringbootServiceregisteryApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServerSpringbootServiceregisteryApplication>(*args)
+}
---
+++ src/test/kotlin/com/nickdferrara/serviceregistery/ServerSpringbootServiceregisteryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nickdferrara.serviceregistery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerSpringbootServiceregisteryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
